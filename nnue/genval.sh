5cfab604c18f7b0ba3f7ec399b4d0d69db9c53c3
Sergio Vieri <sergio.vieri.hp@gmail.com>
2020-07-23 23:02:22 +0800

    Update scripts

--- nnue/genval.sh
+++ nnue/genval.sh
@@ -6,7 +6,7 @@ options="setoption name Threads value $threads
 setoption name Hash value 32768
 setoption name SyzygyPath value /temp/sergio-v/syzygy/6/:/temp/sergio-v/syzygy/3-4-5/
 isready
-gensfen depth 12 depth2 14 loop 10000 random_move_count 5 random_move_maxply 40 write_minply 10 use_draw_in_training_data_generation 1 eval_limit 10000 output_file_name genval/$(date +"%Y%m%d-%H%M")_${HOSTNAME}.bin
+gensfen depth 12 depth2 14 loop 10000 random_move_count 8 random_move_maxply 9 write_minply 11 use_draw_in_training_data_generation 1 eval_limit 10000 output_file_name genval/$(date +"%Y%m%d-%H%M")_${HOSTNAME}.bin
 quit
 "
 
